22c4ffa04b37bf87bfc595068f69b2db77673e4a
jassa66223 <51172269+this_is_jassa@users.noreply.github.com>
2020-05-20 14:58:06 -0400

    s

--- backend/src/middlewares/shared.mid.js
+++ backend/src/middlewares/shared.mid.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const config = require('../../config/config.js');
 const admin = require('firebase-admin');
+const logs_project = require('../model/projectLogs.model');
 
 module.exports = {
 
@@ -11,17 +12,17 @@ module.exports = {
 
         if (!token) { res.status(500).json({ success: false, "msg": "Access denied, No token provided." }); return; }
 
-        
+
         admin.auth().verifyIdToken(token)
-            .then( (decodedToken) => {
+            .then((decodedToken) => {
 
                 let uid = decodedToken.uid;
-                req.user = {userName: uid};
+                req.user = { userName: uid };
                 next();
 
             })
-            .catch( (error) => {
-               
+            .catch((error) => {
+
                 req.user = null;
                 console.error(error)
                 next();
@@ -33,5 +34,36 @@ module.exports = {
     activateRoute: (req, res, next) => {
         res.status(200).send(!!req.user);
     },
+
+    projectLogs: async (req, res, next) => {
+
+        try {
+
+            if (!req.user.permission) {
+                res.status(400).json({ success: false, msg: "Permission Not granted" });
+                return;
+            }
+
+            const {_id} = req.user.project;
+            const {type, message} = req.user.project.log;
+            const {data} = req.res | '';
+
+            const logPayload = {
+                projectId: _id,
+                type: type,
+                message: message,
+                timeStamp: Date.now()
+            }
+
+            const log = await logs_project.create(logPayload);
+            res.status(200).json({log: log, data: data, success: true});
+
+        }
+        catch (err) {
+            console.log(err);
+            res.status(200).json({success: false, message: 'Server Err'});
+
+        }
+    }
 
 }
